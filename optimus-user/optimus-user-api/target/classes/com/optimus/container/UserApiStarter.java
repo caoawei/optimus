314e84df1a6fd4dc7959f674eb2d15ba9dc299c4
caoawei <2585199219@qq.com>
2018-07-24 17:34:04 +0800

    添加日志处理工程

--- optimus-user/optimus-user-api/target/classes/com/optimus/container/UserApiStarter.java
+++ optimus-user/optimus-user-api/target/classes/com/optimus/container/UserApiStarter.java
@@ -17,6 +17,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @MapperScan(basePackages = "com.*")
 public class UserApiStarter {
     public static void main(String[] args) throws Exception {
-        ResetApiStarter.start(MethodHandles.lookup().lookupClass());
+        //ResetApiStarter.start(MethodHandles.lookup().lookupClass());
+
+        System.out.println(UserApiStarter.class.getClassLoader().getResource(""));
+        System.out.println(UserApiStarter.class.getResource(""));
     }
 }
